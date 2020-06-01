74f4dc99baebe3730a53348743f7368bc14a7185
ActonMartin <wszcla@outlook.com>
2020-06-01 16:15:14 +0800

    16:15

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -26,21 +26,21 @@ class MnistDataLoader(BaseDataLoader):
 
 class ISBIDataLoader(BaseDataLoader):
     def __init__(self, data_dir, batch_size, shuffle, validation_split, num_workers):
+        trsfm = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize([0.5], [0.5])
+        ])
         self.data_dir = data_dir
-        self.dataset = ISBIDataSet(self.data_dir)
+        self.dataset = ISBIDataSet(self.data_dir, trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
 
 
 class ISBIDataSet(Dataset):
-    def __init__(self, data_path):
+    def __init__(self, data_path, transform=None):
         # 初始化函数，读取所有data_path下的图片
         self.data_path = data_path
         self.imgs_path = glob.glob(os.path.join(data_path, 'image/*.png'))
-
-    def augment(self, image, flipCode):
-        # 使用cv2.flip进行数据增强，filpCode为1水平翻转，0垂直翻转，-1水平+垂直翻转
-        flip = cv2.flip(image, flipCode)
-        return flip
+        self.transform = transform
 
     def __getitem__(self, index):
         # 根据index读取图片
@@ -48,22 +48,10 @@ class ISBIDataSet(Dataset):
         # 根据image_path生成label_path
         label_path = image_path.replace('image', 'label')
         # 读取训练图片和标签图片
-        image = cv2.imread(image_path)
-        label = cv2.imread(label_path)
-        # 将数据转为单通道的图片
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        label = cv2.cvtColor(label, cv2.COLOR_BGR2GRAY)
-        image = image.reshape(1, image.shape[0], image.shape[1])
-        label = label.reshape(1, label.shape[0], label.shape[1])
-        # image = image / 255
-        # 处理标签，将像素值为255的改为1
-        if label.max() > 1:
-            label = label / 255
-        # 随机进行数据增强，为2时不做处理
-        flipCode = random.choice([-1, 0, 1, 2])
-        if flipCode != 2:
-            image = self.augment(image, flipCode)
-            label = self.augment(label, flipCode)
+        image = Image.open(image_path).convert('1')
+        label = Image.open(label_path).convert('1')
+        image = self.transform(image)
+        label = self.transform(label)
         return image, label
 
     def __len__(self):
